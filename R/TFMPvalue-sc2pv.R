e77ae2956341bffd63d2833d72d65ee45e3d4293
Ge Tan <gtan@me.com>
2014-03-26 17:43:55 +0000

    finish sc2pv

---
+++ R/TFMPvalue-sc2pv.R
@@ -0,0 +1,13 @@
+
+
+TFMsc2pv <- function(mat, score, bg=c(A=0.25, C=0.25, G=0.25, T=0.25),
+                    type=c("PFM", "PWM")){
+  ## TODD: valiate these mat, bg
+  type <- match.arg(type)
+  if(length(score) != 1L){
+    stop("score must be length of 1")
+  }
+  pvalue <- .Call("sc2pv", mat, score, bg, type, PACKAGE="TFMPvalue")
+  return(pvalue)
+}
+
