42141a51b504959180aa2c4554fe7c2d5ec0ebb4
Ge Tan <gtan@me.com>
2022-10-19 23:28:31 +0200

    version 0.0.9
    Remove the invalid UTF-8

--- src/FileException.cpp
+++ src/FileException.cpp
@@ -2,7 +2,7 @@
  *  FileException.cpp
  *  pvalue
  *
- *  Created by Jean-Stéphane Varré on 02/07/07.
+ *  Created by Jean-Stphane Varr on 02/07/07.
  *  Copyright 2007 LIFL-USTL-INRIA. All rights reserved.
  *
  */
